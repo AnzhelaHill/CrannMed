69c657689a676b92dc9face4fa2a52be7ba81361
Anzhela <gilevichanz@gmail.com>
2024-12-27 16:30:12 +0100

    Added eventListener to document DOM

--- js/main.js
+++ js/main.js
@@ -1,29 +1,13 @@
 const allNavItems = document.querySelectorAll(".nav-item")
-const dropdownMenu = document.querySelectorAll(".dropdown-menu")
-// const body = document.querySelector("body")
 
-allNavItems.forEach(item => {
-	const dropdownContent = item.querySelector(".dropdown-menu")
-	item.addEventListener("click", () => {
-		dropdownContent.classList.toggle("dropdown-menu-toggle")
-		allNavItems.forEach(otherItem => {
-			if (otherItem !== item) {
-				otherItem
-					.querySelector(".dropdown-menu")
-					.classList.remove("dropdown-menu-toggle")
-			}
-		})
+document.addEventListener('click', (e) => {
+	allNavItems.forEach(item => {
+		const navLink = item.querySelector(".nav-link")
+		const dropdownContent = item.querySelector(".dropdown-menu")
+		if(e.target == item || e.target == navLink) {
+			dropdownContent.classList.toggle("dropdown-menu-toggle")
+		} else {
+			dropdownContent.classList.remove("dropdown-menu-toggle")
+		}
 	})
-})
-
-// function closeNavigation() {
-// 	allNavItems.forEach(item => {
-// 	    item.querySelector('.dropdown-menu').classList.remove('dropdown-menu-toggle')
-// 	})
-// }
-
-// body.addEventListener('click', closeNavigation)
-
-// document.addEventListener('click', (e) => {
-//     console.log(e)
-//   });
+  });
