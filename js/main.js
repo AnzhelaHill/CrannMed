62f5f97966d0327bec5e07be75a5b4daab3f08bf
Anzhela <gilevichanz@gmail.com>
2025-01-07 14:32:57 +0100

    Finished navigation

--- js/main.js
+++ js/main.js
@@ -2,18 +2,20 @@ const allNavItems = document.querySelectorAll(".nav-item")
 const burgerBtn = document.querySelector(".burger-btn")
 const navbarBtnsBurger = document.querySelector(".navbar-btns-burger")
 const navXmark = document.querySelector(".nav-xmark")
+const AllNavBurgerItems = document.querySelectorAll(".nav-item-burger")
 
-document.addEventListener('click', (e) => {
+ const OpenMainNav = (e) => {
 	allNavItems.forEach(item => {
 		const navLink = item.querySelector(".nav-link")
+		const navLinkImg = item.querySelector(".nav-link img")
 		const dropdownContent = item.querySelector(".dropdown-menu")
-		if(e.target == item || e.target == navLink) {
+		if(e.target == item || e.target == navLink || e.target == navLinkImg) {
 			dropdownContent.classList.toggle("dropdown-menu-toggle")
 		} else {
 			dropdownContent.classList.remove("dropdown-menu-toggle")
 		}
 	})
-  });
+  };
 
 const OpenBurgerNav = () => {
 	navbarBtnsBurger.style.display = 'flex';
@@ -23,5 +25,22 @@ const CloseBurgerNav = () => {
 	navbarBtnsBurger.style.display = 'none';
 }
 
+const OpenBurgerNavLinks = (e) => {
+	AllNavBurgerItems.forEach(item => {
+		const navLink = item.querySelector('.nav-link-burger')	
+		const navLinkImg = item.querySelector('.nav-link-burger img')
+		const dropdownContent = item.querySelector('.dropdown-menu-burger')
+		if(e.target == navLink || e.target == navLinkImg) {
+			dropdownContent.classList.toggle("dropdown-menu-burger-toggle")
+			navLink.classList.toggle("nav-link-burger-toggle")
+		} else {
+			dropdownContent.classList.remove("dropdown-menu-burger-toggle")
+			navLink.classList.remove("nav-link-burger-toggle")
+		}
+	})
+}
+
+document.addEventListener('click', OpenMainNav)
 burgerBtn.addEventListener('click', OpenBurgerNav)
 navXmark.addEventListener('click', CloseBurgerNav)
+document.addEventListener('click', OpenBurgerNavLinks)
