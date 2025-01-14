aaaef98615366f1b00e56577212f3bee78899279
Anzhela <gilevichanz@gmail.com>
2025-01-14 14:47:43 +0100

    Changed function PushDownBtnCtaItem

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ const allCtaItems = document.querySelectorAll(".cta-item")
 const allPhotoItems = document.querySelectorAll(".photo-item")
 const arrowUpGalery = document.querySelector(".arrow-up")
 const arrowDownGalery = document.querySelector(".arrow-down")
-const progressLine = document.querySelector(":root")
 
  const OpenMainNav = (e) => {
 	allNavItems.forEach(item => {
@@ -21,15 +20,12 @@ const progressLine = document.querySelector(":root")
 		}
 	})
   };
-
 const OpenBurgerNav = () => {
 	navbarBtnsBurger.style.display = 'flex';
 }
-
 const CloseBurgerNav = () => {
 	navbarBtnsBurger.style.display = 'none';
 }
-
 const OpenBurgerNavLinks = (e) => {
 	allNavBurgerItems.forEach(item => {
 		const navLink = item.querySelector('.nav-link-burger')	
@@ -45,43 +41,69 @@ const OpenBurgerNavLinks = (e) => {
 	})
 }
 
+const sizeOfOneElement = 100 / allCtaItems.length
+const root = document.documentElement
 const PushDownBtnCtaItem = () => {
 	for (let i = 0; i < allCtaItems.length; i++) {
+		let x = sizeOfOneElement * (i+1)
 		if (!allCtaItems[i].className.includes('inactive')) {
 			allCtaItems[i].classList.add('inactive')
+			allCtaItems[i].classList.remove('active')
 			allPhotoItems[i].classList.add('inactive')
+			allPhotoItems[i].classList.remove('active')
+			// allCtaItems[i].addEventListener('transitionend', () => {
+			// })
+			allPhotoItems[i].style.display = 'none'
+				allCtaItems[i].style.display = 'none'
+				allCtaItems[i].style.opacity = '0'
+			console.log(allCtaItems[0])
+
 			if (i == allCtaItems.length-1) {
 				allCtaItems[0].classList.remove('inactive')
 				allPhotoItems[0].classList.remove('inactive')
+				root.style.setProperty('--progress-top', '0%')
+				allPhotoItems[0].style.display = 'flex'
+				allCtaItems[0].style.display = 'flex'
+				allCtaItems[0].style.opacity = '1'
+
 			} else {
 				allCtaItems[i+1].classList.remove('inactive')
 				allPhotoItems[i+1].classList.remove('inactive')
+				root.style.setProperty('--progress-top', x + '%')
+				// allCtaItems[i+1].addEventListener('transitionend', () => {
+				// })
+				allPhotoItems[i+1].style.display = 'flex'
+				allCtaItems[i+1].style.display = 'flex'
+				allCtaItems[i+1].style.opacity = '1'
+			console.log(allCtaItems[i+1])
+				break
 			}
 			break
 		}
 	}
 }
-
 const PushUpBtnCtaItem = () => {
 	for (let i = 0; i < allCtaItems.length; i++) {
+		let x = sizeOfOneElement * (i-1)
 		if (!allCtaItems[i].className.includes('inactive')) {
 			allCtaItems[i].classList.add('inactive')
 			allPhotoItems[i].classList.add('inactive')
 			if (i == 0) {
 				allCtaItems[allCtaItems.length-1].classList.remove('inactive')
 				allPhotoItems[allCtaItems.length-1].classList.remove('inactive')
+				x = sizeOfOneElement * (allCtaItems.length-1) 
+				root.style.setProperty('--progress-top', x + '%')
 			} else {
 				allCtaItems[i-1].classList.remove('inactive')
 				allPhotoItems[i-1].classList.remove('inactive')
+				root.style.setProperty('--progress-top', x + '%')
 			}
 			break
 		}
 	}
 }
 
-const prog = () => {
-	console.log('click')
-}
+
 
 document.addEventListener('click', OpenMainNav)
 burgerBtn.addEventListener('click', OpenBurgerNav)
