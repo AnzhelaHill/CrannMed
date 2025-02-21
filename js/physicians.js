ebb05922df93761295b27f31e293edb9eef011a4
Anzhela <gilevichanz@gmail.com>
2025-02-21 15:02:50 +0100

    Added section contact to page Clinical Results

---
+++ js/physicians.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const faqItems = document.querySelector(".faq-items")
+    const faqItemAll = document.querySelectorAll(".faq-item")
+
+    const OpenAnswer = (e) => {
+        faqItemAll.forEach(item => {
+            const faqAnswer = item.querySelector('.faq-answer')
+            const faqQuestion = item.querySelector('.faq-question')
+            const iQuestion = faqQuestion.querySelector('.fa-solid')
+            const pQuestion = faqQuestion.querySelector('p')
+
+            const OpenList = () => {
+                faqAnswer.style.maxHeight = "400px"
+                faqQuestion.style.backgroundColor = "#20c7c7"
+                faqQuestion.style.borderColor = "#20c7c7"
+                iQuestion.style.rotate = "180deg"
+            }
+            const CloseList = () => {
+                faqAnswer.style.maxHeight = "0px"
+                faqQuestion.style.backgroundColor = "#fff"
+                faqQuestion.style.borderColor = "var(--main-color)"
+                iQuestion.style.rotate = "0deg"
+            }
+            if(e.target == faqQuestion || e.target == iQuestion || e.target == pQuestion) {
+                faqQuestion.classList.toggle('hover')
+                faqAnswer.classList.toggle('active')
+                if (faqAnswer.classList.contains('active')) {
+                    OpenList()
+                } else {
+                    CloseList()
+                }
+            } else {
+                faqQuestion.classList.remove('hover')
+                faqAnswer.classList.remove('active')
+                CloseList()
+            }    
+            if (!faqQuestion.classList.contains('hover')) {
+                faqQuestion.addEventListener('mouseover', () => {
+                    faqQuestion.style.backgroundColor = '#20c7c727'
+                })  
+                faqQuestion.addEventListener('mouseout', () => { 
+                    faqQuestion.style.backgroundColor = '#fff'
+                })
+            } else {
+                faqQuestion.addEventListener('mouseover', () => {
+                    faqQuestion.style.backgroundColor = '#20c7c7'
+                })  
+                faqQuestion.addEventListener('mouseout', () => { 
+                    faqQuestion.style.backgroundColor = '#20c7c7'
+                })
+            }
+            
+        }
+    )}
+    
+    document.addEventListener('click', OpenAnswer)
+})
